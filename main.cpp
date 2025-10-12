83a1dea96f040760369d4d5e6289d82543e2dd8d
Hooollis <25721009@bjtu.edu.cn>
2025-10-12 18:17:26 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    // 读取BGR图像
+    Mat img_bgr = imread("test.png");
+    if(img_bgr.empty()) {
+        std::cerr << "Error: Could not load image" << std::endl;
+        return -1;
+    }
+
+    // 转换到HSV颜色空间
+    Mat img_hsv;
+    cvtColor(img_bgr, img_hsv, COLOR_BGR2HSV);
+
+    // 分离HSV通道
+    std::vector<Mat> hsv_channels;
+    split(img_hsv, hsv_channels);
+    Mat img_h = hsv_channels[0];
+    Mat img_s = hsv_channels[1];
+    Mat img_v = hsv_channels[2];
+
+    // 创建H通道掩码
+    Mat mask_h, mask_s, mask_v;
+    inRange(img_h, 120, 175, mask_h);
+    inRange(img_s, 50, 255, mask_s);
+    inRange(img_v, 50, 255, mask_v);
+
+    // 组合H和S通道掩码
+    Mat mask_h_and_s;
+    bitwise_and(mask_h, mask_s, mask_h_and_s);
+
+    // 组合H、S和V通道掩码
+    Mat mask;
+    bitwise_and(mask_h_and_s, mask_v, mask);
+
+    // 应用掩码到原图
+    Mat img_out;
+    bitwise_and(img_bgr, img_bgr, img_out, mask);
+
+    // 显示和保存结果
+    imshow("img", img_out);
+    imwrite("img_out.png", img_out);
+    waitKey(0);
+    
+    return 0;
+}
