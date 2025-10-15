8ca2a2c2d4599f34d388ea7a21d67832c7fb006f
Hooollis <25721009@bjtu.edu.cn>
2025-10-15 22:57:44 +0800

    first commit

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ int main()
 {
     // 读取BGR图像
     Mat img_bgr = imread("test.png");
-    if(img_bgr.empty()) {
+    if(img_bgr.empty() == true) {
         std::cerr << "Error: Could not load image" << std::endl;
         return -1;
     }
+    imshow("img", img_bgr);
 
     // 转换到HSV颜色空间
     Mat img_hsv;
@@ -23,12 +24,18 @@ int main()
     Mat img_h = hsv_channels[0];
     Mat img_s = hsv_channels[1];
     Mat img_v = hsv_channels[2];
+    imshow("img_h", img_h);
+    imshow("img_s", img_s);
+    imshow("img_v", img_v);
 
     // 创建H通道掩码
     Mat mask_h, mask_s, mask_v;
     inRange(img_h, 120, 175, mask_h);
     inRange(img_s, 50, 255, mask_s);
     inRange(img_v, 50, 255, mask_v);
+    imshow("mask_h", mask_h);
+    imshow("mask_s", mask_s);
+    imshow("mask_v", mask_s);
 
     // 组合H和S通道掩码
     Mat mask_h_and_s;
@@ -37,13 +44,14 @@ int main()
     // 组合H、S和V通道掩码
     Mat mask;
     bitwise_and(mask_h_and_s, mask_v, mask);
+    imshow("mask", mask_s);
 
     // 应用掩码到原图
     Mat img_out;
     bitwise_and(img_bgr, img_bgr, img_out, mask);
 
     // 显示和保存结果
-    imshow("img", img_out);
+    imshow("img_out", img_out);
     imwrite("img_out.png", img_out);
     waitKey(0);
     
